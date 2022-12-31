b932ad72d84c4cd458d252db0153badafc185319
akhilkirantest <akhilkirank@gmail.com>
2022-12-31 14:11:03 +0530

    Delete waste stuff

--- Sound2.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-MyText = 'hi Ayush, you are a genius. We are going out today. The lake seems to be a great place. are you coming ?'
-def SpeakText(command):
-     
-    # Initialize the engine
-    engine = pyttsx3.init()
-    engine.say(command)
-    engine.runAndWait()
-SpeakText(MyText)
--- mine.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-time.sleep(15)
-for x in range(1,3):
-    pyautogui.moveRel(0, 500, duration = 1)
-    pyautogui.leftClick(duration = 3)
-    pyautogui.moveRel(0, -500, duration = 1)
-    pyautogui.leftClick(duration = 3)
